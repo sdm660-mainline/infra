da9f977e40c4a9f1231888e5f419bfd50f0f061c
Alexey Minnekhanov <alexeymin@minlexx.ru>
2025-09-22 02:05:00 +0300

    Small updates for U-Boot runner image
    * make it possible to create image for another arch
    * preinstall new build deps for U-Boot runner image
    * remove useless hostname setting - it was not working
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@minlexx.ru>

--- docker-github-runner/00_download_runner_archive.sh
+++ docker-github-runner/00_download_runner_archive.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-VER=2.325.0
-curl -O -L https://github.com/actions/runner/releases/download/v${VER}/actions-runner-linux-arm64-${VER}.tar.gz
-ln -sf actions-runner-linux-arm64-${VER}.tar.gz actions-runner-linux-arm64.tar.gz
+VER=2.328.0
+ARCH=x64
+curl -o actions-runner-linux-${ARCH}-${VER}.tar.gz -L https://github.com/actions/runner/releases/download/v${VER}/actions-runner-linux-${ARCH}-${VER}.tar.gz
+ln -sf actions-runner-linux-${ARCH}-${VER}.tar.gz actions-runner-linux.tar.gz
