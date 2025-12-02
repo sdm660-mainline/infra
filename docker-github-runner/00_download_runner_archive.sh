993fe3d508b3ec0b2c613f89b0a6a5b0a957ff5b
Alexey Minnekhanov <alexeymin@minlexx.ru>
2025-12-02 03:32:28 +0300

    Current actions-runner release is 2.330
    Signed-off-by: Alexey Minnekhanov <alexeymin@minlexx.ru>

--- docker-github-runner/00_download_runner_archive.sh
+++ docker-github-runner/00_download_runner_archive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VER=2.328.0
+VER=2.330.0
 ARCH=x64
 curl -o actions-runner-linux-${ARCH}-${VER}.tar.gz -L https://github.com/actions/runner/releases/download/v${VER}/actions-runner-linux-${ARCH}-${VER}.tar.gz
 ln -sf actions-runner-linux-${ARCH}-${VER}.tar.gz actions-runner-linux.tar.gz
