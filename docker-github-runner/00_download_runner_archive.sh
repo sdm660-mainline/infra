5103ed6c2344ddb23605fb3f2e589d733ab90b55
Alexey Minnekhanov <alexeymin@minlexx.ru>
2026-06-25 16:35:04 +0300

    Switch images to Ubuntu 26.04 and more
    * 00_download: separate arch downloads
    * 01_build_image: separate arch scripts
    * Dockerfile.x64: renamed from Dockerfile
    * install more build tools and deps for pmOS
      kernel builds (clang)
    * switch to regular sudo (doas is dead)
    * use local pmbootstrap mirror, because pmOS
      gitlab is sometimes not reachable
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@minlexx.ru>

--- docker-github-runner/00_download_runner_archive.sh
+++ docker-github-runner/00_download_runner_archive.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-VER=2.330.0
-ARCH=x64
-curl -o actions-runner-linux-${ARCH}-${VER}.tar.gz -L https://github.com/actions/runner/releases/download/v${VER}/actions-runner-linux-${ARCH}-${VER}.tar.gz
-ln -sf actions-runner-linux-${ARCH}-${VER}.tar.gz actions-runner-linux.tar.gz
+
+VER=2.335.1
+
+for ARCH in x64 arm64; do
+	curl -o actions-runner-linux-${ARCH}-${VER}.tar.gz -L https://github.com/actions/runner/releases/download/v${VER}/actions-runner-linux-${ARCH}-${VER}.tar.gz
+	ln -sf actions-runner-linux-${ARCH}-${VER}.tar.gz actions-runner-linux-${ARCH}.tar.gz
+done
